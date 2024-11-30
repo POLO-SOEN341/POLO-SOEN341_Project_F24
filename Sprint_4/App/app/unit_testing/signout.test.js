3d68582c2b3adca3ba6b64f53205eba1b333c235
Alvin Biju <bijualvin2@gmail.com>
2024-11-30 18:55:51 -0500

    Update signout.test.js
    Signed-off-by: Alvin Biju <bijualvin2@gmail.com>

--- Sprint_4/App/app/unit_testing/signout.test.js
+++ Sprint_4/App/app/unit_testing/signout.test.js
@@ -1,4 +1,4 @@
-import React from 'react';  // Import React
+import React from 'react';  
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import SignOutButton from '../components/Disconnect.js';
